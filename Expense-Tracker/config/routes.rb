46b090e9b6c8e71bd9f3b829e0ec84d95ba4baf0
rtalrejagammastack <rtalreja@grepruby.io>
2023-03-20 20:04:19 +0530

    Initial Commit

---
+++ Expense-Tracker/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get 'users',to: 'devise/registrations#new'
+  end
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
