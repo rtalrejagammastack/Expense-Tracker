a252c59bd3487da488a0a947f476f4c5225ad9d9
rtalrejagammastack <rtalreja@grepruby.io>
2023-03-27 11:57:51 +0530

    Expense Category

--- app/models/expense_sub_category.rb
+++ app/models/expense_sub_category.rb
@@ -2,6 +2,8 @@
 
 # Model to store all the sub-categories of any categories.
 class ExpenseSubCategory < ApplicationRecord
+  validates :name, presence: true, uniqueness: { scope: :category_id, message: "%{value} already exists" }
+  
   belongs_to :category, class_name: 'ExpenseCategory'
-  belongs_to :user
+  # belongs_to :user
 end
